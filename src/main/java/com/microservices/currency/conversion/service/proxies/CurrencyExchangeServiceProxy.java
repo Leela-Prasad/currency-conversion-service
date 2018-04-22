89d83b19f8fbdae48cbdae529afa59c133e290c7
Leela-Prasad <leela.jagu@gmail.com>
2018-04-22 20:03:31 +0530

    Added LoadBalancing using Ribbon.
    In this riboon will load balance between currency exchange service instances, as of now these instances are hardcorded in application.properties but in next branches we will get these instance info from Naming server(Eureka).

--- src/main/java/com/microservices/currency/conversion/service/proxies/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/microservices/currency/conversion/service/proxies/CurrencyExchangeServiceProxy.java
@@ -1,5 +1,6 @@
 package com.microservices.currency.conversion.service.proxies;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +13,11 @@ import com.microservices.currency.conversion.service.models.CurrencyConversionBe
  * details of a microservice.
  *
  */
-@FeignClient(name= "xyz",url="localhost:8000")
+@FeignClient(name= "xyz")
+// Ribbon Client will get instance
+// details from this property(xyz.ribbon.listOfServers) 
+//defined in application.properties
+@RibbonClient(name="xyz")
 public interface CurrencyExchangeServiceProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
